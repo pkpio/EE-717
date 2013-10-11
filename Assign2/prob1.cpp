81b435ccc16582952b1f88b26ae5c7810dd5f2da
Praveen Kumar Pendyala <praveendath92@gmail.com>
2013-10-10 21:56:26 -0700

    Prob1: printTree() function working
    This will be used as a test bench while building other functions

--- Assign2/prob1.cpp
+++ Assign2/prob1.cpp
@@ -5,6 +5,7 @@ It has pointers to left, right childs and parent
 **********************************************************/
 
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 //Define a node which supports binary tree
@@ -24,11 +25,11 @@ node *fLeaf = NULL; //Last leaf that is filled
 //Assumed max # of root nodes as 50. Just to make Queue implementation easy
 #define width 50
 
-node* queue[width];
+node *queue[width];
 int qPtr = 0;
 int qFrntPtr = 0;	//For implementing Queues using circular array
 
-void enqueue(node* val){
+void enqueue(node *val){
 	if(qPtr != width){
 		queue[qPtr] = val;
 		qPtr++;
@@ -37,8 +38,8 @@ void enqueue(node* val){
 	}
 }
 
-int dequeue(){
-	node* val;
+node * dequeue(){
+	node *val;
 	if(qFrntPtr < qPtr){
 		val = queue[qFrntPtr];
 		queue[qFrntPtr] = 0;
@@ -52,10 +53,13 @@ int dequeue(){
 }
 
 bool qEmpty(){
-	if(qPtr == qPtr)
-		return true
-	else
-		return false
+    bool output = false;
+	if(qPtr == qFrntPtr){
+		output = true;
+	} else{
+		output = false;
+    }
+    return output;
 }
 
 
@@ -75,7 +79,7 @@ void Insert(int key){
 			fLeaf = temp;
 		} 
 		//Case 2: Leaf node has no sibling
-		else if((fLeaf->parent)->RChild == NULL){
+		else if((fLeaf->parent)->rChild == NULL){
 			fLeaf->rChild = temp;
 			temp->parent = fLeaf;
 			fLeaf = temp;
@@ -116,43 +120,109 @@ void Insert(int key){
 }
 
 void printHeap(){
-	node* temp = root;
+	node *temp = root;
 	cout<<"The current heap is: "<<endl;
 	cout<<temp->key<<endl;
 	enqueue(temp->lChild);
 	enqueue(temp->rChild);
 
-	i = 0; //to insert a line break
-	while(!qEmpty){
+	int i = 1; //to insert a line break
+	int j = 1;
+	while(qPtr != qFrntPtr){
 		temp = dequeue();
 		cout<<temp->key<<" ";
 
-		if(temp->lChild != NULL)
+		if(temp->lChild != NULL){
 			enqueue(temp->lChild);
-		if(temp->rChild != NULL)
+		}
+		if(temp->rChild != NULL){
 			enqueue(temp->rChild);
+		}
 
 		//Below lines are just to ensure a line break after a level
 		i++;
-		if(i%2 == 0)
+		if((i+1)%((int)pow(2.0, j)) == 0){
 			cout<<endl;
+			j++;
+		}
 	}
  }
 
 
  /******************************** Main program ********************************/
 
-node *temp = new node;
-temp->key = 3;
+int main(){
+    
+    // Test nodes...........
+	node *temp = new node;
+	temp->key = 10;
+	temp->lChild = NULL;
+	temp->rChild = NULL;
 
-node *temp1 = new node;
-temp1->key = 2;
+	node *temp1 = new node;
+	temp1->key = 9;
+	temp1->lChild = NULL;
+	temp1->rChild = NULL;
 
-node *temp2 = new node;
-temp2->key = 1;
+	node *temp2 = new node;
+	temp2->key = 8;
+	temp2->lChild = NULL;
+	temp2->rChild = NULL;
 
-root = temp;
-root->lChild = temp1;
-root->rChild = temp2;
+	node *temp3 = new node;
+	temp3->key = 7;
+	temp3->lChild = NULL;
+	temp3->rChild = NULL;
 
-printHeap();
+	node *temp4 = new node;
+	temp4->key = 6;
+	temp4->lChild = NULL;
+	temp4->rChild = NULL;
+
+	node *temp5 = new node;
+	temp5->key = 5;
+	temp5->lChild = NULL;
+	temp5->rChild = NULL;
+
+	node *temp6 = new node;
+	temp6->key = 4;
+	temp6->lChild = NULL;
+	temp6->rChild = NULL;
+
+	node *temp7 = new node;
+	temp7->key = 14;
+	temp7->lChild = NULL;
+	temp7->rChild = NULL;
+
+	node *temp8 = new node;
+	temp8->key = 15;
+	temp8->lChild = NULL;
+	temp8->rChild = NULL;
+
+	node *temp9 = new node;
+	temp9->key = 16;
+	temp9->lChild = NULL;
+	temp9->rChild = NULL;
+
+	node *temp10 = new node;
+	temp10->key = 17;
+	temp10->lChild = NULL;
+	temp10->rChild = NULL;
+
+	root = temp;
+	root->lChild = temp1;
+	root->rChild = temp2;
+	temp1->lChild = temp3;
+	temp1->rChild = temp4;
+	temp2->lChild = temp5;
+	temp2->rChild = temp6;
+	temp3->lChild = temp7;
+	temp3->rChild = temp8;
+	temp4->lChild = temp9;
+	temp4->rChild = temp10;
+
+	printHeap();
+
+	int c;
+	cin>>c;
+}
